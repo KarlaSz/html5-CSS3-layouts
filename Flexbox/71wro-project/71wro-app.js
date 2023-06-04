d54bd62af64eb9fd901e461fe9d230665eb84492
KarlaSz <128708615+KarlaSz@users.noreply.github.com>
2023-06-04 20:07:14 +0200

    Update 71wro-app.js

--- Flexbox/71wro-project/71wro-app.js
+++ Flexbox/71wro-project/71wro-app.js
@@ -7,10 +7,10 @@ const ARROW_RIGHT = document.querySelector(".popup__arrow--right");
 
 let currentImgIndex;
 
-const showNextImgIndex = () => {
+const showNextImg = () => {
         if (currentImgIndex === THUMBNAILS.length - 1) {
             currentImgIndex = 0;
-        }else {
+        } else {
             currentImgIndex = currentImgIndex + 1;
         }
         POPUP_IMG.src = THUMBNAILS[currentImgIndex].src;
@@ -19,35 +19,61 @@ const showNextImgIndex = () => {
 const showPreviousImg = () => {
     if (currentImgIndex === 0) {
         currentImgIndex = THUMBNAILS.length - 1;
-    }else {
+    } else {
         currentImgIndex--;
     }
     POPUP_IMG.src = THUMBNAILS[currentImgIndex].src;
 };
 
+const closePopUp = () => {
+    POPUP.classList.add("fade-out");
+    setTimeout(() => {
+        POPUP.classList.add("hidden");
+        POPUP.classList.remove("fade-out");
+    }, 300);
+};
 
-THUMBNAILS.forEach( (thumbnail, index) => {
-    thumbnail.addEventListener("click", (e) =>  {
+THUMBNAILS.forEach((thumbnail, index) => {
+    const showPopUp = (e) => {
         POPUP.classList.remove("hidden");
         POPUP_IMG.src = e.target.src;
         currentImgIndex = index;
+    };
+
+    thumbnail.addEventListener("click", showPopUp);
+
+    thumbnail.addEventListener("keydown", (e) => {
+        if (e.code === "Enter" || e.keyCode === 13) {
+            showPopUp(e);
+        }
+        
     });
 });
 
-POPUP_CLOSE.addEventListener("click", () => {
-     POPUP.classList.add("hidden");
-});
+POPUP_CLOSE.addEventListener("click", closePopUp);
 
-ARROW_RIGHT.addEventListener("click", showNextImgIndex);
+ARROW_RIGHT.addEventListener("click", showNextImg);
 
 ARROW_LEFT.addEventListener("click", showPreviousImg);
 
 document.addEventListener("keydown", (e) => {
-    if (e.key === "ArrowRight" || e.keyCode === 39 ) {
-        showNextImgIndex();
+    if (!POPUP.classList.contains("hidden")) {
+        if (e.code === "ArrowRight" || e.keyCode === 39) {
+            showNextImg();
+        }
+       
+        if (e.code === "ArrowLeft" || e.keyCode === 37) {
+            showPreviousImg();
+        }
+        if (e.code === "Escape" || e.keyCode === 27) {
+            closePopUp();
+        }
     }
-   
-    if (e.code === "ArrowLeft" || e.keyCode === 37 ) {
-        showPreviousImg();
+    
+});
+
+POPUP.addEventListener("click", (e) => {
+    if (e.target === POPUP) {
+        closePopUp();
     }
 });
